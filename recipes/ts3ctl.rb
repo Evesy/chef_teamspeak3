f0ace86222600af326b951987c7c6a6ec49ff629
evesy <meves23@gmail.com>
2016-03-08 01:28:11 +0000

    Run a daemon reload if systemd files are changed

--- recipes/ts3ctl.rb
+++ recipes/ts3ctl.rb
@@ -2,9 +2,17 @@
 template "/usr/lib/systemd/system/teamspeak.service" do
         source "ts3_service.erb"
         mode "0644"
+	notifies :run, "execute[daemon-reload]", :immediately
 end
 
 # Add teamspeak to startup
 service "teamspeak" do
 	action :enable
 end
+
+# Reload daemon
+execute "daemon-reload" do
+	command "systemctl daemon-reload"
+	action :nothing
+end
+
